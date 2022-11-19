72846553964a1bb17636c96f4bfc84003fc18208
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-19 17:44:17 +0530

    added product class with same methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+        	return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y *z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String args[]){
+        Product obj = new Product();
+
+        int twoIntProducts = obj.product(10, 200);
+        int threeIntProducts = obj.product(55, 9, 21);
+	   	  double twoDoubleProducts = obj.product(23.78d, 52.11d);
+        
+		    System.out.println(twoIntProducts);
+	      System.out.println(threeIntProducts);
+		    System.out.println(twoDoubleProducts);
+    }
 }
